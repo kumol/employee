b1be979c62125dd7a891f4bf2c95f2872b7dd431
kumol <kumol01763@gmail.com>
2024-07-17 23:38:22 +0600

    include git ignore

--- backend/server/models/db.js
+++ backend/server/models/db.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-
-console.log(process.env.DB_URL)
 mongoose.connect(process.env.DB_URL)
 .then((v)=> console.log("db connected successfully"))
 .catch(err=>console.log(err))
