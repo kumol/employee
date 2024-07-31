e23b7b9f75027ca772d9a50e4b4db3d119d4d76a
kumol <kumol01763@gmail.com>
2024-07-31 23:23:53 +0600

    main div

--- frontend/src/Components/EmployeeManagementApp.js
+++ frontend/src/Components/EmployeeManagementApp.js
@@ -1,7 +1,9 @@
 function EmployeeManagementApp(){
-    
+
     return (
-        <p>EmployeeManagementApp</p>
+        <div className="d-flex flex-column justify-content-center align-items-center w-100 p-10 ">
+
+        </div>
     );
 };
 
