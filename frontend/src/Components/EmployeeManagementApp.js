fb2fa17fa02dd3884f7ab9244ca2098a5c45203d
kumol <kumol01763@gmail.com>
2024-07-31 23:28:42 +0600

    new employee add button

--- frontend/src/Components/EmployeeManagementApp.js
+++ frontend/src/Components/EmployeeManagementApp.js
@@ -2,7 +2,16 @@ function EmployeeManagementApp(){
 
     return (
         <div className="d-flex flex-column justify-content-center align-items-center w-100 p-10 ">
-
+            <h1> Employee Management App</h1>
+            <div className="d-flex justify-content-center w-100">
+                <div className="w-80 bg-light border p3"> 
+                    <div className="d-flex justify-content-center mb-5">
+                        <button className="btn btn-primary">
+                            Add
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
